1960e98010391038172c3cc5f56ce92f773c7373
Cristian Puncea <cristian@Cristians-MacBook-Air.local>
2023-11-30 17:35:45 +0200

    adding a modal to show for invalid data and add functionality to cancel button

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import React, { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
-function NewProject({ onAdd }) {
+function NewProject({ onAdd, onCancel }) {
+  const modal = useRef();
+
   const inputTitle = useRef();
   const inputDescription = useRef();
   const inputDueDate = useRef();
@@ -16,34 +19,62 @@ function NewProject({ onAdd }) {
       description: enteredDescription,
       dueDate: enteredDueDate,
     };
-    // validation
+
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
 
     onAdd(enteredProjectData);
   };
 
+  const handleCancel = () => {
+    onCancel();
+  };
+
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={inputTitle} label="Title" />
-        <Input ref={inputDescription} label="Description" textarea />
-        <Input type="date" ref={inputDueDate} label="Due Date" />
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2 className="text-xl font-bold text-stone-700 mt-4 mby-4">
+          Invalid Input
+        </h2>
+        <p className="text-stone-600 mb-4">
+          Oops... looks like you forgot to enter a value.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button
+              className="text-stone-800 hover:text-stone-950"
+              onClick={handleCancel}
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={inputTitle} label="Title" />
+          <Input ref={inputDescription} label="Description" textarea />
+          <Input type="date" ref={inputDueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
