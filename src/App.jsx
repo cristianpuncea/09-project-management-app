c9cca519014fe24da738635825fc13f8c4131c40
Cristian Puncea <cristian@Cristians-MacBook-Air.local>
2023-11-30 14:01:19 +0200

    install and configure eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import React, { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProject: undefined,
+    projects: [],
+  });
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
