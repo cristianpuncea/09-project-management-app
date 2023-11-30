ea13f9501e538927707b4feb7719fc828a3663b8
Cristian Puncea <cristian@Cristians-MacBook-Air.local>
2023-11-30 17:02:03 +0200

    adding saved projects in the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function App() {
     setProjectsState((prevState) => {
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
@@ -41,7 +42,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
